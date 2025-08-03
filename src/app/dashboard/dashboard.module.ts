66730d14a9a105da19201dd71da51573c0756c5f
navaneethbonthu <navaneethbonthu@gmail.com>
2025-08-03 13:46:11 +0530

    udpated

---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CreateTaskComponent } from './create-task/create-task.component';
+import { DashboardComponent } from './dashboard.component';
+import { TaskDetailsComponent } from './task-details/task-details.component';
+import { CommonModule } from '@angular/common';
+
+import { SharedModule } from '../shared.module';
+import { OverviewComponent } from './overview/overview.component';
+import { RouteModule } from '../route.module';
+import { RouterModule } from '@angular/router';
+import { StatsComponent } from './stats/stats.component';
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    CreateTaskComponent,
+    TaskDetailsComponent,
+    OverviewComponent,
+    StatsComponent,
+  ],
+  imports: [CommonModule, SharedModule, RouteModule, RouterModule],
+  exports: [
+    DashboardComponent,
+    CreateTaskComponent,
+    TaskDetailsComponent,
+    SharedModule,
+  ],
+})
+export class DashboardModule {}
